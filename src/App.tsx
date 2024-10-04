1a3f43aa0e715b3a9f7bf0c1fb445ba05e37c29b
mohamed <mohamedyasserxd449@gamil.com>
2024-10-04 12:31:33 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,67 @@
 {/* router */}
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 {/* components */}
-// import Header from './components/Header/Header';
-// import NavbarMobile from './components/NavbarMobile/NavbarMobile';
-// import Aside from './components/Aside/Aside';
-// import LangBox from './components/LangBox/LangBox';
-// import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import NavbarMobile from './components/NavbarMobile/NavbarMobile';
+import Aside from './components/Aside/Aside';
+import LangBox from './components/LangBox/LangBox';
+import Footer from './components/Footer/Footer';
 
 {/* pages */}
-// import Home from './pages/Home/Home';
-// import Docs from './pages/Docs/Docs';
-// import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import Docs from './pages/Docs/Docs';
+import About from './pages/About/About';
 // import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
-    <>jgherjjgherg</>
-    // <Router>
-    //   <Routes>
-    //     <Route path='/' element={
-    //       <>
-    //         <NavbarMobile />
-    //         <main className="main">
-    //           <Aside />
-    //           <div className="article">
-    //             <Header title="الصفحة الرئيسية" />
-    //             <Home />
-    //             <Footer />
-    //           </div>
-    //           <LangBox />
-    //         </main>
-    //       </>
-    //     } />
-    //     <Route path='/docs' element={
-    //       <>
-    //         <NavbarMobile />
-    //         <main className="main">
-    //           <Aside />
-    //           <div className="article">
-    //             <Header title="مستندات" />
-    //             <Docs />
-    //             <Footer />
-    //           </div>
-    //           <LangBox />
-    //         </main>
-    //       </>
-    //     } />
-    //     <Route path='/about' element={
-    //       <>
-    //         <NavbarMobile />
-    //         <main className="main">
-    //           <Aside />
-    //           <div className="article">
-    //             <About />
-    //             <Footer />
-    //           </div>
-    //           <LangBox />
-    //         </main>
-    //       </>
-    //     } />
-    //     <Route path='*' element={<NotFound />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <NavbarMobile />
+            <main className="main">
+              <Aside />
+              <div className="article">
+                <Header title="الصفحة الرئيسية" />
+                <Home />
+                <Footer />
+              </div>
+              <LangBox />
+            </main>
+          </>
+        } />
+        <Route path='/docs' element={
+          <>
+            <NavbarMobile />
+            <main className="main">
+              <Aside />
+              <div className="article">
+                <Header title="مستندات" />
+                <Docs />
+                <Footer />
+              </div>
+              <LangBox />
+            </main>
+          </>
+        } />
+        <Route path='/about' element={
+          <>
+            <NavbarMobile />
+            <main className="main">
+              <Aside />
+              <div className="article">
+                <About />
+                <Footer />
+              </div>
+              <LangBox />
+            </main>
+          </>
+        } />
+        {/* <Route path='*' element={<NotFound />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
